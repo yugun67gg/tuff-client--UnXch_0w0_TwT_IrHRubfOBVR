e4a032cdb6fdc5b28da35604f66ae9a82d1e86ed
minUnXch <419105350@qq.com>
2026-04-18 09:45:02 -0700

    Delete oneblock-menu-integration.js

--- oneblock-menu-integration.js
+++
@@ -1,3 +0,0 @@
-// OneBlock Menu Integration is not needed for Eaglercraft Canvas menus
-// The oneblock-loader.js handles all configuration
-console.log('[OneBlockMenu] Canvas menu detected - no DOM manipulation needed');
